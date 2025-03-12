1965e49122a251d1b06f13784172e33d19ab4138
alenorgue <alenoriegag@gmail.com>
2025-03-12 12:11:27 +0100

    falta ejercicio 12

--- ex13-bonus-switch.js
+++ ex13-bonus-switch.js
@@ -9,6 +9,30 @@
  *
  */
 
-let mes = 4;
+let mes = 2;
 
 // Escribir tu código a partir de aquí
+switch (mes) {
+    case 1:
+    case 2:
+    case 3:
+        console.log("Nos encontramos en el primer trimestre");
+        break;
+    case 4:
+    case 5:
+    case 6:
+        console.log("Nos encontramos en el segundo trimestre");
+        break;
+    case 7:
+    case 8:
+    case 9:
+        console.log("Nos encontramos en el tercer trimestre");
+        break;
+    case 10:
+    case 11:
+    case 12:
+        console.log("Nos encontramos en el cuarto trimestre");
+        break;
+    default:
+        console.log("Mes inválido");
+}
--- mi-script.js
+++ mi-script.js
@@ -13,15 +13,17 @@
 let numeroInicial = 0;
 let numeroFinal = 10;
 // Elimina las 3 líneas de  código siguiente y codifica la solución
+for (let i = numeroInicial; i < numeroFinal; i++) {
 
-dibujarCirculo();
-dibujarCirculo();
-dibujarCirculo();
+        dibujarCirculo(); }
+
 
 /**
  * BONUS: Modifica el bucle para introducir la variable 'tocaHexagono'. El bucle debe funcionar como antes, pero ahora, cuando nos encotremos en la iteración número 'tocaHexagono', se debe pintar un hexagono amarillo. Investiga el fichero "mi-script.js" para averiguar como podríamos pintar un hexágono
  */
-let tocaHexagono = 4;
+/** 
+ * 
+ */
 
 /**
  * Ejemplo: si numeroInicial=0, numeroFinal=10, y tocaHexagono=4, deberíamos pintar:
